42875dbb3420231235debf9496097d862cbba65d
MohamedOsmanX <hamarsaysaab1@gmail.com>
2026-01-20 18:07:00 +0400

    hid the api key

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   static const String baseUrl ="https://api.openweathermap.org/data/2.5";
-  static const String appId = '9311b7d5dce5bbdf6e134c7bd9e47aae';
+  static const String appId = 'addYourApiKey';
 }
